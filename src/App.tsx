cc7809e4e3c7c87718c564355c4f16d81defbd28
jpms_rahman <rahman@jpms.co.jp>
2024-12-03 17:39:51 +0900

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,27 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-    const { signOut } = useAuthenticator();
+    //const { signOut } = useAuthenticator();
     const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+    const [name, setName] = useState<string>("");
+
+    const { user, signOut } = useAuthenticator();
 
     useEffect(() => {
         client.models.Todo.observeQuery().subscribe({
             next: (data) => setTodos([...data.items]),
         });
-    }, []);
+
+        const loginID = user?.signInDetails?.loginId;
+        if (loginID) {
+            const index = loginID.indexOf("@");
+            if (index > -1) {
+                const name = loginID.substring(0, index);
+                setName(name);
+            }
+        }
+    }, [user]);
 
     function createTodo() {
         client.models.Todo.create({ content: window.prompt("Todo content") });
@@ -24,21 +37,21 @@ function App() {
     }
     return (
         <main>
+            <h1>{name}</h1>
             <button onClick={signOut}>Sign out</button>
             <h1>My todos</h1>
             <button onClick={createTodo}>+ new</button>
             <ul>
                 {todos.map((todo) => (
-                    <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
-                        {todo.content}
-                    </li>
+                    <>
+                        {todo.content && todo.content.length > 0 && (
+                            <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
+                                {todo.content}
+                            </li>
+                        )}
+                    </>
                 ))}
             </ul>
-            <div>
-                🥳 App successfully hosted. Try creating a new todo.
-                <br />
-                <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">Review next step of this tutorial.</a>
-            </div>
         </main>
     );
 }
