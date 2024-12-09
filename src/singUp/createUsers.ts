f8061140015a9a80f0137ddc961230187ef0ade8
jpms_rahman <rahman@jpms.co.jp>
2024-12-09 17:20:38 +0900

    fix4

--- src/singUp/createUsers.ts
+++ src/singUp/createUsers.ts
@@ -1,19 +1,17 @@
 import * as AWS from "@aws-sdk/client-cognito-identity-provider";
 
-import config from "../../amplify_outputs.json";
-
 export class CreateAppUser{
   private userPool: string = "";
- // private region: string = "";
+  private region: string = "";
   
     constructor(upool: string) {
       this.userPool = upool;
-     // this.region = "ap-southeast-2"; 
+      this.region = "ap-southeast-2"; 
     }
  
   
   public async createInviteUserMutation(email: string, familyName: string, givenName: string) {
-    const client = new AWS.CognitoIdentityProviderClient({ ...config });
+    const client = new AWS.CognitoIdentityProviderClient({ region: this.region });
     const input: AWS.AdminCreateUserCommandInput = {
       UserPoolId: this.userPool,
       Username: email,
