31dec1a975937639e2e3e3cb3adad39f245fdcbc
jpms_rahman <rahman@jpms.co.jp>
2024-12-10 12:04:52 +0900

    trigger

--- amplify/lamda-function/handler.ts
+++ amplify/lamda-function/handler.ts
@@ -1,9 +1,10 @@
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const handler = async (event:any) => {
-    if (event.triggerSource === "CustomMessage_SignUp") {
-        return "ユーザー作成成功、メッセージ送信!";
-    }
+    // if (event.triggerSource === "CustomMessage_SignUp") {
+    //     return "ユーザー作成成功、メッセージ送信!";
+    // }
   
-    return "failed....";
+    console.log(event);
+     return "Test... ";
 };
