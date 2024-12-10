8710dfc62aacb961bd89e534e0dd9d507021d152
jpms_rahman <rahman@jpms.co.jp>
2024-12-10 11:51:04 +0900

    Trigger remove

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { triggerFunction } from './lamda-function/triggerFuntion';
+//import { triggerFunction } from './lamda-function/triggerFuntion';
 
 defineBackend({
   auth,
   data,
-  triggerFunction,
+  //triggerFunction,
 });
